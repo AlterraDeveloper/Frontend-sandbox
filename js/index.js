fe12e8a76108d33dcb0af41b3ac4518167d2d8bf
Eugene Kiselev <jkiselev2411@gmail.com>
2020-11-16 22:27:18 +0600

    просмотрел первую часть 6 часового видео

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// const firstName = 'Eugene'
+// const lastName = 'Kiselev'
+
+// let x = 5
+// x **= 2
+// console.log();
+
+// let check = (posNum, answer) => {
+//     console.log('check' + posNum);
+//     return answer;
+// }
+
+// const cars = ['Mazda', 'Mersedes', 'Ford']
+// const cars = new Array('Mazda', 'Mersedes', 'Ford')
+// for (const car of cars) {
+//     console.log(car);
+// }
+
+const person = {
+    firstName: 'Eugene',
+    lastName: 'Kiselev',
+    languages: ['ru', 'en', 'ky'],
+    hasWife: true,
+    greet: function () {
+        console.log($ 'Hello) My name is {{$firstName}}')
+    }
+}
+
+person.greet();
