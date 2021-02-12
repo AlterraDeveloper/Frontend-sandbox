3657659a52a5917defe5890fad2f107075411f13
Eugene Kiselev <jkiselev2411@gmail.com>
2021-02-12 16:24:29 +0600

    Сложный javascript простыми словами. урок 2: контекст this. как работают bind, call, apply

--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
-const person = new Object({
-    name: 'maxim',
-    age: 25,
-    greet: function () {
-        console.log('Greet!');
+function hello(params) {
+    console.log('Hello', this);
+}
+
+const person = {
+    name: 'Eugene',
+    age: 22,
+    sayHello: hello,
+    sayHelloWindow: hello.bind(window),
+    logInfo: function (job) {
+        console.group(`${this.name} info:`)
+        console.log(`Name is ${this.name}`);
+        console.log(`Age is ${this.age}`);
+        console.log(`Job is ${job}`);
+        console.groupEnd()
     }
-})
+}
 
-Object.prototype.sayHello = () => console.log('Hello!');
+const lena = {
+    name: 'Elena',
+    age: 23
+}
 
-const lena = Object.create(person)
-lena.name = 'Elena'
+// person.logInfo.bind(lena, 'manager')()
+// person.logInfo.call(lena, 'manager')
+// person.logInfo.apply(lena, ['manager'])
 
-const str = new String('I am string')
+const array = [1, 2, 3, 4, 5]
+
+// function multBy(arr, n) {
+//     return arr.map(i => i * n)
+// }
+
+Array.prototype.multBy = function(n) {
+    return this.map(i => i * n)
+}
+
+console.log(array.multBy(15))
